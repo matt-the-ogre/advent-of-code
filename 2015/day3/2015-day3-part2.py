ace30406e919aa746e18eac136617ebe86bdcd8d
Matt Manuel <matt.manuel@microsoft.com>
2021-11-24 14:20:52 -0800

    completed 2015 day 3 part 2

--- 2015/day3/2015-day3-part2.py
+++ 2015/day3/2015-day3-part2.py
@@ -15,7 +15,20 @@
 # ^>v< delivers presents to 4 houses in a square, including twice to the house at his starting/ending location.
 # ^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2 houses.
 
-import os
+# --- Part Two ---
+# The next year, to speed up the process, Santa creates a robot version of himself, Robo-Santa, to deliver presents with him.
+
+# Santa and Robo-Santa start at the same location (delivering two presents to the same starting house), then take turns moving based on instructions from the elf, who is eggnoggedly reading from the same script as the previous year.
+
+# This year, how many houses receive at least one present?
+
+# For example:
+
+# ^v delivers presents to 3 houses, because Santa goes north, and then Robo-Santa goes south.
+# ^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back where they started.
+# ^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction and Robo-Santa going the other.
+
+# import os
 
 # cwd = os.getcwd()  # Get the current working directory (cwd)
 # files = os.listdir(cwd)  # Get all the files in that directory
@@ -42,6 +55,14 @@ if DEBUG:
 x = 0
 y = 0
 
+# start with Santa moving, then Robo-Santa, and alternate
+thisMove = "Santa"
+SantaX = 0
+SantaY = 0
+
+RoboSantaX = 0
+RoboSantaY = 0
+
 # add the first house to the dictionary in position "0x0" with a value of 1 present
 houses = {str(x)+"x"+str(y) : 1}
 
@@ -52,7 +73,18 @@ if DEBUG:
 
 for step in moves[0]:
     if DEBUG:
-        print(step)
+        print(thisMove,step)
+
+    # set x and y based on whose move it is
+    if thisMove == "Santa":
+        x = SantaX
+        y = SantaY
+    elif thisMove == "RoboSanta":
+        x = RoboSantaX
+        y = RoboSantaY
+    else:
+        print("Error: unknown value for 'thisMove': ", thisMove)
+    
     # based on the character of the step, increment or decrement x or y
     if step == "^":
         y += 1
@@ -66,7 +98,22 @@ for step in moves[0]:
         print("Error: Unrecognized step type: ", step)
         break
     
+    # set the position of Santa or RoboSanta based on the updated move
+    if thisMove == "Santa":
+        # store the current coordinates for Santa (for his next move)
+        SantaX = x
+        SantaY = y
+        # switch to RoboSanta for the next iteration through the loop
+        thisMove = "RoboSanta"
+    elif thisMove == "RoboSanta":
+        RoboSantaX = x
+        RoboSantaY = y
+        thisMove = "Santa"
+    else:
+        print("Error: unknow value for 'thisMove': ", thisMove)
+    
     currentHouse = str(x)+"x"+str(y)
+
     if DEBUG:
         print(currentHouse)
     
